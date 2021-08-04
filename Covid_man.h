09ca252468abc8cf716400bb45caa98f60efea0b
Ronald Gutierrez Arratia <73799699+121-rrg@users.noreply.github.com>
2021-08-04 17:18:10 -0500

    Add files via upload

--- Covid_man.h
+++ Covid_man.h
@@ -2,10 +2,10 @@
 #include "Enfermo.h"
 class Covid_man : public Personaje{
     private:
-    SAMPLE *muerte=nullptr;
-    BITMAP *pacbmp=nullptr;//Creara un espacio en donde estara pacman
-    BITMAP *muertebmp=nullptr;
-    BITMAP *pacman=nullptr;//Dimensiones de un pacman;//Imagen pacman
+    SAMPLE *muerte=NULL;
+    BITMAP *pacbmp=NULL;//Creara un espacio en donde estara pacman
+    BITMAP *muertebmp=NULL;
+    BITMAP *pacman=NULL;//Dimensiones de un pacman;//Imagen pacman
     int dir;//para que pacman no se mueva al iniciar el juego
     int px,py;//posicion adecuada de pacman
     int numMapa;
@@ -31,6 +31,7 @@ class Covid_man : public Personaje{
     void inicioMuerte();
     void finalMuerte(int);
     void atajo(int);//
+    void ganarVida(int puntos);
 
 };
 SAMPLE* Covid_man::getMusicMuerte(){
@@ -70,12 +71,25 @@ void Covid_man::finalMuerte(int i){
 const char* Covid_man::imprimirVida(int vida){
     if(vida==0)
         return "0";
+        
     else if(vida==1)
         return "1";
+        
     else if(vida ==2)
         return "2";
+        
     else if(vida ==3)
         return "3";
+        
+    else if(vida ==4)
+        return "4";
+        
+    else if(vida ==5)
+        return "5";
+        
+    else if(vida ==6)
+        return "6";
+        
     return "error";
 }
 void Covid_man::movimiento(){
@@ -217,149 +231,15 @@ int Covid_man::getVida(){
 void Covid_man::setVida(int vidas){
    this->vidas=vidas;
 }
-
-
-
-
-
-
-
-
-//Enfermo
-/*#include <cstdlib>
-class Enfermo{
-	private:
-    BITMAP *enemigobmp=load_bitmap("enemigo.bmp",NULL);;//Creara un espacio en donde estara el enemigo
-    BITMAP *enemigo=create_bitmap(30,30);//Dimensiones de un pacman;//Imagen del enemigo
-    int E_dir=0;
-    int E_x=30*14,E_y=30*15; //coordenadas	 en el mapa
-    int numMapa=1;
-    public:
-    int getPosX();
-    int getPosY();
-    void setNumMapa(int);
-    void dibujarEnemigo();
-    void movimientoEnemigo();
-    void atajo(int);
-
-};
-void Enfermo::setNumMapa(int numMapa){
-   this->numMapa=numMapa;
+void Covid_man::ganarVida(int puntos){
+    if(puntos%100==0 && puntos!=0){
+        vidas++;
+    }
 }
-int Enfermo::getPosX(){
-   return E_x;
-}
-int Enfermo::getPosY(){
-   return E_y;
-}
-void Enfermo::dibujarEnemigo(){
-	blit(enemigobmp,enemigo,0,0,0,0,30,30);
-    draw_sprite(buffer.buffer,enemigo,E_x,E_y);//para respetar transparencia
-}
-void Enfermo::movimientoEnemigo(){
-	Enfermo::dibujarEnemigo();
 
-	if(numMapa==1){
-        if (E_dir==0){
-		   if( mapa1 [E_y/30][(E_x-30)/30] != 'X')
-			  E_x-=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-        }
-	    else if(E_dir==1){
-		   if( mapa1 [E_y/30][(E_x+30)/30] != 'X')
-			  E_x+=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-	       }
 
-	    else if(E_dir==2){
-		   if( mapa1 [(E_y-30)/30][E_x/30] != 'X')
-			  E_y-=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-	    }
 
-	    else if(E_dir==3){
-		   if( mapa1 [(E_y+30)/30][E_x/30] != 'X')
-			  E_y+=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-	    }
-	}//fin numMapa==1
-	else if(numMapa==2){
-        if (E_dir==0){
-		   if( mapa2 [E_y/30][(E_x-30)/30] != 'X')
-			  E_x-=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-        }
-	    else if(E_dir==1){
-		   if( mapa2 [E_y/30][(E_x+30)/30] != 'X')
-			  E_x+=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-	       }
 
-	    else if(E_dir==2){
-		   if( mapa2 [(E_y-30)/30][E_x/30] != 'X')
-			  E_y-=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-	    }
 
-	    else if(E_dir==3){
-		   if( mapa2 [(E_y+30)/30][E_x/30] != 'X')
-			  E_y+=30;
-		   else
-			  E_dir=rand()%4; //valor al azar entre 0 y 3
-	    }
-	}//fin numMapa==2
-	// movimiento para los atajos
-	Enfermo::atajo(numMapa);
-}
-void Enfermo::atajo(int numMapa){
-    if(numMapa==1){//Deinicion del atajo del mapa 1
-        if(E_x<=-30 && E_y==270){
-            E_x=870;E_y=270;
-        }
-        else if(E_x>=870 && E_y==270){
-            E_x=-30;E_y=270;
-        }
-        else if(E_x<=-30 && E_y==540){
-            E_x=870;E_y=30;
-        }
-        else if(E_x>=870 && E_y==30){
-            E_x=-30;E_y=540;
-        }
-        else if(E_x>=870 && E_y==330){
-            E_x=870;E_y=540;
-        }
-        else if(E_x>=870 && E_y==540){
-            E_x=870;E_y=330;
-        }
-   }
-   else if(numMapa==2){
-        if(E_x<=-30 && E_y==270){
-            E_x=870;E_y=270;
-        }
-        else if(E_x>=870 && E_y==270){
-            E_x=-30;E_y=270;
-        }
-        else if(E_x<=-30 && E_y==540){
-            E_x=870;E_y=30;
-        }
-        else if(E_x>=870 && E_y==30){
-            E_x=-30;E_y=540;
-        }
-        else if(E_x>=870 && E_y==330){
-            E_x=870;E_y=540;
-            E_dir=0;//Para que vaya en la direccion correcta ya que el taajo entrada y salida estan en el mismo muro
-        }
-        else if(E_x>=870 && E_y==540){
-            E_x=870;E_y=330;
-            E_dir=0;//Para que vaya en la direccion correcta
-        }
-   }
-}*/
+
 
